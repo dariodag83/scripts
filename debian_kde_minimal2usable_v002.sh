c86ad007111035c63f4be20780696640cf7a0b8b
Dario D'Agostino <134972638+dariodag83@users.noreply.github.com>
2025-11-11 01:35:51 +0100

    Update debian_kde_minimal2usable_v002.sh

--- debian_kde_minimal2usable_v002.sh
+++ debian_kde_minimal2usable_v002.sh
@@ -52,6 +52,11 @@ apt install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-fra tesseract-ocr-i
 # Kleopartra
 apt install -y kleopatra
 
+
+#Finalize
+systemctl disable ifupdown
+systemctl reboot
+
 # Chrome
 wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./chrome.deb
