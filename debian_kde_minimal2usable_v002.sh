34c978c1f953ffe5a6b188bba8226ad854f42d0d
Dario D'Agostino <134972638+dariodag83@users.noreply.github.com>
2025-11-11 01:36:44 +0100

    Update debian_kde_minimal2usable_v002.sh

--- debian_kde_minimal2usable_v002.sh
+++ debian_kde_minimal2usable_v002.sh
@@ -52,11 +52,6 @@ apt install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-fra tesseract-ocr-i
 # Kleopartra
 apt install -y kleopatra
 
-
-#Finalize
-systemctl disable ifupdown
-systemctl reboot
-
 # Chrome
 wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./chrome.deb
@@ -92,3 +87,8 @@ apt install -y onlyoffice-desktopeditors onlyoffice-desktopeditors-help
 
 # Paraview
 apt install -y paraview
+
+
+#Finalize
+systemctl disable ifupdown
+systemctl reboot
